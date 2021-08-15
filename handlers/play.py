621444ba0cb8acbc237674a0837d4704037d9623
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-15 20:25:13 +0530

    Create play.py

--- handlers/play.py
+++ handlers/play.py
@@ -123,7 +123,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
     image1 = Image.open("./background.png")
 
-    image2 = Image.open("image/Musiqo.png")
+    image2 = Image.open("image/sticker.png")
 
     image3 = changeImageSize(1280, 720, image1)
 
