a4156d527da9f453c8b46c3062c94037fe1b2340
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-15 20:19:23 +0530

    Create pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -6,5 +6,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"[✨](https://telegra.ph/file/c6c36a4bcecef600fd9be.jpg) Heya! This is the Music Assistant of [Musiqo](https://t.me/MusiqoRobot) Ask at @Unitedbotsupport")
+  await USER.send_message(message.chat.id,"[💝](https://telegra.ph/file/36233dc2948a5d44293ea.jpg) Heya! This is the Music Assistant of [Parvathy](https://t.me/Parvathy_MusicRobot) Ask at [SUPPORT GROUP](t.me/noobiezhub)")
   return                        
